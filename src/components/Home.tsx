99fb91b4ca6026c18da68404fec8091c48412971
Oskar Nesheim <nesheimo@gmail.com>
2023-09-28 15:20:15 +0200

    add: first iteration pokemon card at homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,30 +1,15 @@
-// import { useQuery } from "@tanstack/react-query";
 import { data } from "../../public/mockData";
 import PokemonCard from "./PokemonCard";
-// import { IPokemonList } from "../interfaces/pokemon";
 
 export default function Home() {
-  //   const { data, error, isLoading } = useQuery<IPokemonList, Error>({
-  //     queryKey: ["first_20_pokemon"],
-  //     queryFn: (): IPokemonList => {
-  //       return data as IPokemonList;
-  //     },
-  //   });
-
-  //   if (isLoading) {
-  //     return <div>Loading...</div>;
-  //   }
-
-  //   if (error) {
-  //     return <div>Error: {error.message}</div>;
-  //   }
-
   return (
     <div className="home">
       <h1>Home</h1>
-      {data.results.map((pokemon) => {
-        return <PokemonCard key={pokemon.name} pokemon={pokemon} />;
-      })}
+      <div className="pokemons_container">
+        {data.results.map((pokemon) => {
+          return <PokemonCard key={pokemon.name} pokemon={pokemon} />;
+        })}
+      </div>
     </div>
   );
 }
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,16 +1,40 @@
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { IPokemon } from "../interfaces/pokemon";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 type PokemonCardProps = {
   pokemon: IPokemon;
 };
 
 export default function PokemonCard({ pokemon }: PokemonCardProps) {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h3>
-        This is <NavLink to={pokemon.name}>{pokemon.name}</NavLink>
-      </h3>
+    <div onClick={() => navigate(pokemon.name)}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {pokemon.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
