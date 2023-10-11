3ee3842a38bbac4a91f3de145002a68772dcd621
Andreas Kjosavik <andrkjos@stud.ntnu.no>
2023-10-11 15:33:17 +0200

    refactor: make global theme
    Refs: #21

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import "./App.css";
 import Navbar from "./components/navbar";
 import { Outlet } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+// https://mui.com/material-ui/customization/default-theme/
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0B4C8D", // blue
+      light: "#ffffff", // white
+      dark: "#0E98F8", // light blue
+    },
+    secondary: {
+      main: "#FFCB05", // yellow
+      light: "#ffffff", // white
+      dark: "#FFCB05", // yellow
+    },
+  },
+  typography: {
+    fontFamily: ["pokemonfont;"].join(","),
+  },
+});
 
 function App() {
   return (
     <>
-      <Navbar />
-      <div className="content_container">
-        <Outlet />
-      </div>
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <div className="content_container">
+          <Outlet />
+        </div>
+      </ThemeProvider>
     </>
   );
 }
--- src/components/TeamMember.tsx
+++ src/components/TeamMember.tsx
@@ -1,36 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { IPokemon } from "../interfaces/pokemon";
 import Typography from "@mui/material/Typography";
-// import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import { Box, createTheme } from "@mui/material";
+import { Box } from "@mui/material";
 
 type PokemonCardProps = {
   name: string;
   selected: boolean;
 };
 
-const customFontStyle = {
-  fontFamily: "pokemonfont", // Use the font-family name you defined
-};
-
 export default function PokemonCard({ name, selected }: PokemonCardProps) {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#0B4C8D", // blue
-        light: "#ffffff", // white
-        dark: "#0E98F8", // light blue
-      },
-      secondary: {
-        main: "#FFCB05", // yellow
-        light: "#ffffff", // white
-        dark: "#FFCB05", // yellow
-      },
-    },
-  });
-
   const { data, error, isLoading } = useQuery<IPokemon, Error>(
     [name, "_pokemon"],
     () => {
@@ -55,14 +35,14 @@ export default function PokemonCard({ name, selected }: PokemonCardProps) {
       sx={{
         width: 300,
         border: selected ? "1px solid #E0F1FF" : "none",
-        backgroundColor: theme.palette.primary.main,
+        backgroundColor: "primary.dark",
         opacity: selected ? 0.9 : 0.6,
         transition: selected
           ? "opacity 0.3s ease-in-out, border 0.3s ease-in-out"
           : "none",
         "&:hover": {
           transition: "background-color 0.3s ease-in-out",
-          backgroundColor: theme.palette.primary.dark,
+          backgroundColor: "primary.dark",
         },
       }}
     >
@@ -83,11 +63,7 @@ export default function PokemonCard({ name, selected }: PokemonCardProps) {
           </Box>
 
           {/* Right side: Text */}
-          <Typography
-            variant="body1"
-            sx={{ color: theme.palette.primary.light }}
-            style={customFontStyle}
-          >
+          <Typography variant="body1" sx={{ color: "primary.light" }}>
             {data.name}
           </Typography>
         </div>
