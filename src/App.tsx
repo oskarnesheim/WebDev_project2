25f2a4eb22adbd4065955f4c7aae5087c0fa38e2
Andreas Kjosavik <andrkjos@stud.ntnu.no>
2023-10-12 12:55:12 +0200

    refactor: move theme settings into own file
    Refs: #21

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import "./App.css";
 import Navbar from "./components/navbar";
 import { Outlet } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-// https://mui.com/material-ui/customization/default-theme/
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#0B4C8D", // blue
-      light: "#ffffff", // white
-      dark: "#0E98F8", // light blue
-    },
-    secondary: {
-      main: "#FFCB05", // yellow
-      light: "#ffffff", // white
-      dark: "#FFCB05", // yellow
-    },
-  },
-  typography: {
-    fontFamily: ["pokemonfont;"].join(","),
-  },
-});
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./Theme";
 
 function App() {
   return (
