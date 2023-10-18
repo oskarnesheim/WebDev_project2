c41c1b7ca718755017be91e6aa7f308bd8ef507c
Andreas Kjosavik <andrkjos@stud.ntnu.no>
2023-10-18 16:06:15 +0200

    feat: add support for both single pokemon and every pokemon
    Now you can get eigter a single pokemon by id, or every pokemon in the list (atm 300)
    
    Refs: #24

--- Server/schema/schema.js
+++ Server/schema/schema.js
@@ -3,6 +3,7 @@ import {
   GraphQLString,
   GraphQLSchema,
   GraphQLInt,
+  GraphQLList,
 } from "graphql";
 
 import data from "../public/data.json" assert { type: "json" };
@@ -21,14 +22,19 @@ const Pokemon = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    pokemons: {
+      type: new GraphQLList(Pokemon),
+      resolve(parent, args) {
+        return data;
+      },
+    },
     pokemon: {
       type: Pokemon,
       args: {
         id: { type: GraphQLInt },
       },
       resolve(parent, args) {
-        const foundPokemon = data.find((pokemon) => pokemon.id == args.id);
-        return foundPokemon ? foundPokemon : null;
+        return data.find((pokemon) => pokemon.id == args.id);
       },
     },
   },
