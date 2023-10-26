fc642d4eaaf107531b64da46e904dae59f00b129
Oskar Nesheim <nesheimo@gmail.com>
2023-10-26 12:51:58 +0200

    feat #28 filtering on mongoose query to database.

--- Server/schema/schema.js
+++ Server/schema/schema.js
@@ -1,7 +1,7 @@
 // Mongoose model
 import { log } from "console";
 import { PokemonModel } from "../models/Pokemon.js";
-import PokemonType from "./GetAllPokemons.js"
+import PokemonType from "./GetAllPokemons.js";
 
 import {
   GraphQLObjectType,
@@ -16,7 +16,7 @@ const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
     pokemons: {
-      args:{
+      args: {
         upperLimit: { type: GraphQLInt },
         lowerLimit: { type: GraphQLInt },
       },
@@ -34,35 +34,41 @@ const RootQuery = new GraphQLObjectType({
         return PokemonModel.findById(args._id);
       },
     },
-    pokemonsSortedAndFiltered:{
-        type: new GraphQLList(PokemonType),
-        args: {
-            filters: { type: GraphQLList(GraphQLString) },
-            sorting: { type: GraphQLList(GraphQLList(GraphQLString)) },
-            range: { type: GraphQLList(GraphQLInt) },
-        },
-        resolve(parent, args) {
-          const filters = args.filters;
-          const sortingMap = new Map();
-          args.sorting.forEach((sort) => {
-            sortingMap.set(sort[0], sort[1]);
-          });
-          const range = args.range;
-          log(filters);
-          log(sortingMap);
-          log(range);
+    pokemonsSortedAndFiltered: {
+      type: new GraphQLList(PokemonType),
+      args: {
+        filters: { type: GraphQLList(GraphQLString) },
+        sorting: { type: GraphQLList(GraphQLList(GraphQLString)) },
+        range: { type: GraphQLList(GraphQLInt) },
+      },
+      resolve(parent, args) {
+        const filters = args.filters;
+        const sortingMap = new Map();
+        args.sorting.forEach((sort) => {
+          sortingMap.set(sort[0], sort[1]);
+        });
+        const range = args.range;
 
-
-          if (filters.length = 0){
-            if (range.length = 2){
-              return PokemonModel.find().sort(sortingMap).skip(range[0]).limit(range[1]);
-            }
-            return PokemonModel.find().sort(sortingMap);
-          }
-          return PokemonModel.find().sort(sortingMap).skip(range[0]).limit(range[1]);
+        if (filters.length === 0) {
+          return PokemonModel.find()
+            .sort(sortingMap)
+            .skip(range[0])
+            .limit(range[1]);
+        }
+        return PokemonModel.find({
+          types: {
+            $elemMatch: {
+              "type.name": { $in: filters },
+            },
+          },
+        })
+          .sort(sortingMap)
+          .skip(range[0])
+          .limit(range[1]);
+      },
     },
   },
-}});
+});
 
 const schema = new GraphQLSchema({
   query: RootQuery,
