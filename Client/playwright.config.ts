4e6f6a7f8bfc67b24669511075f91233efea8d30
Andreas Kjosavik <andrkjos@stud.ntnu.no>
2023-11-15 15:33:48 +0100

    remove mobile tests

--- Client/playwright.config.ts
+++ Client/playwright.config.ts
@@ -44,20 +44,20 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] }, //, headless: false },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
     /* Test against mobile viewports. */
-    {
-      name: "Mobile Chrome",
-      use: { ...devices["Pixel 5"] },
-    },
-    {
-      name: "Mobile Safari",
-      use: { ...devices["iPhone 12"] },
-    },
+    // {
+    //   name: "Mobile Chrome",
+    //   use: { ...devices["Pixel 5"] },
+    // },
+    // {
+    //   name: "Mobile Safari",
+    //   use: { ...devices["iPhone 12"] },
+    // },
   ],
 
   /* Run your local dev server before starting the tests */
